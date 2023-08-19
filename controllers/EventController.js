88f3ff16fd246298c47d856814a37ae24e1aa0b3
pemauric <pedro.a.mauricio1@gmail.com>
2023-08-19 12:13:38 -0300

    added js in EventController and event.routes

---
+++ controllers/EventController.js
@@ -0,0 +1,38 @@
+
+const Event  = require('../models/Event')
+
+const db = require('../db/conn');
+
+module.exports = class EventController {
+    
+    static async showEvents (req, res){
+        
+    }
+
+    static createEvent (req, res) {
+        
+    }
+    
+    static createEventPost (req, res) {
+        
+    }
+
+    static async getEvent (req, res) {
+        
+    }
+
+
+    static async editEvent (req, res) {
+        
+    }
+
+    static async removeEvent (req, res) {
+        
+    }
+
+    static async updateEventPost (req, res) { 
+        
+        
+    }
+
+}
